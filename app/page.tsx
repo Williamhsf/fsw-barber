c4f71a8ffa96203ebbf629cf165a7ebd47fc7558
Williamhsf <williamhenriquefb@gmail.com>
2024-08-06 23:16:27 -0300

    chore: inital page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
+import Image from "next/image"
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Input } from "./_components/ui/input"
 import { Button } from "./_components/ui/button"
 
 const Home = () => {
-  return <Button>test</Button>
+  return (
+    <div>
+      {/* header*/}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, William</h2>
+        <p>Segunda-feira, 05 de Agosto.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca" />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+            alt="Agende nos melhores com FSW Barber"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
